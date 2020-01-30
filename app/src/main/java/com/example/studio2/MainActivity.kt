fe9632cdd450c665e5b063acdf9247bc2046e46d
elijah.pena@wustl.edu <elijah.pena@wustl.edu>
2020-01-30 14:57:00 -0600

    Genius work right here

--- app/src/main/java/com/example/studio2/MainActivity.kt
+++ app/src/main/java/com/example/studio2/MainActivity.kt
@@ -3,12 +3,14 @@ package com.example.studio2
 import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.Button
 import android.widget.TextView
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
+    private var CHANGE_REQUEST = 1
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -16,4 +18,8 @@ class MainActivity : AppCompatActivity() {
 
     }
 
+    fun getChange(view: View) {
+        val addChangeIntent = Intent(this, ChangeActivity::class.java)
+        startActivityForResult(addChangeIntent, CHANGE_REQUEST)
+    }
 }
